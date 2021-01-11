ba7a356ef3d9be8ab790294fa5d905e33219389b
satodd1967 <seth.todd@outlook.com>
2021-01-11 09:36:13 -0600

    Added a renderUser method tot he User class and updated the fetch method in index.js.

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-    debugger;
+    fetchUsers()
 })
+
+const BASE_URL = "http://127.0.0.1:3000"
+
+function fetchUsers(){
+    fetch(`${BASE_URL}/users`)
+    .then(resp => resp.json())
+    .then(users => {
+        for (let user of users){
+            let u = new User(user.id, user.email, user.username)
+            u.renderUser();
+        }
+    })
+}
--- frontend/src/user.js
+++ frontend/src/user.js
@@ -4,4 +4,16 @@ class User{
         this.email = email;
         this.username = username;
     }
+
+    renderUser() {
+        let usersDiv = document.getElementById("users-container")
+
+        usersDiv.innerHTML +=
+        `
+        <ul>
+        <h3>Username: ${this.username}</h3>
+        <li>Email: ${this.email}</li>
+        </ul>
+        `
+    }
 }
