d6d01564bce4e6302ade649793e3cb42575acbf1
satodd1967 <seth.todd@outlook.com>
2021-01-10 18:31:56 -0600

    added exceptions for password digest, created at and updated at to the controllers file

--- backend/fitness/app/controllers/users_controller.rb
+++ backend/fitness/app/controllers/users_controller.rb
@@ -5,12 +5,12 @@ class UsersController < ApplicationController
   def index
     @users = User.all
 
-    render json: @users
+    render json: @users, :except => [:password_digest, :created_at, :updated_at]
   end
 
   # GET /users/1
   def show
-    render json: @user
+    render json: @user, :except => [:password_digest, :created_at, :updated_at]
   end
 
   # POST /users
